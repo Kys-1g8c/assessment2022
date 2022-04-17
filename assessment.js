d9cfc39e74e2aea6c10e9994f1b4b8ed2dbcf0b5
Kys-1g8c <86555864+Kys-1g8c@users.noreply.github.com>
2022-04-17 17:11:11 +0900

    Bootstrapの適用

--- assessment.js
+++ assessment.js
@@ -37,16 +37,31 @@ assessmentButton.onclick = () => {
   }
   console.log('ボタンが押されました');
 
-  // TODO 診断結果表示エリアの作成
+  // 初期化処理
   resultDivided.innerText = '';
-  const header = document.createElement('h3');
-  header.innerText = '診断結果';
-  resultDivided.appendChild(header);
+
+  // headerDivided の作成
+  const headerDivided = document.createElement('div');
+  headerDivided.setAttribute('class', 'card-header');
+  headerDivided.innerText = '診断結果';
+
+  // bodyDivided の作成
+  const bodyDivided = document.createElement('div');
+  bodyDivided.setAttribute('class', 'card-body');
 
   const paragraph = document.createElement('p');
-  const result = assessment(userName);
-  paragraph.innerText = result;
-  resultDivided.appendChild(paragraph);
+  paragraph.setAttribute('class', 'card-text');
+  paragraph.innerText = assessment(userName);
+  const result = paragraph.innerText;
+  bodyDivided.appendChild(paragraph);
+
+  // resultDivided に Bootstrap のスタイルを適用する
+  resultDivided.setAttribute('class', 'card');
+  resultDivided.setAttribute('style', 'max-width: 700px;')
+
+  // headerDivided と bodyDivided を resultDivided に差し込む
+  resultDivided.appendChild(headerDivided);
+  resultDivided.appendChild(bodyDivided);
 
   // TODO ツイートエリアの作成
   tweetDivided.innerText = '';
@@ -62,7 +77,6 @@ assessmentButton.onclick = () => {
   anchor.innerText = 'Tweet #あなたのいいところ';
 
   tweetDivided.appendChild(anchor);
-
 }
 
 /**
